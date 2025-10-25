2dbd10645efd5bbc79212cca7ba2a3a71badc350
Tarik <tarik.jaidani@gmail.com>
2025-10-25 21:36:07 +0200

    1

--- Cpp_Level5.cpp
+++ Cpp_Level5.cpp
@@ -1,20 +1,41 @@
-// Cpp_Level5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+void PrintHeader() {
+	cout << "\t\t\tMultiplication Table From 1 to 10\t\t\t" << endl;
+	cout << endl;
+
+	for (int i = 1; i <= 10; i++) {
+		cout << "\t" << i ;
+	}
+	cout << endl;
+	cout << "------------------------------------------------------------------------------------"<< endl;
+}
+
+string Column(int i) {
+	if ( i < 10) 
+		return " |";
+	else
+		return "|";
+}
+
+void printTable() {
+	for (int i = 1; i <= 10; i++) {
+		 
+		cout << i << " " << Column(i);
+		for (int j = 1; j <= 10; j++) {
+		 
+			cout << "\t" << i * j  ;
+			 
+		}
+		cout << endl;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	PrintHeader();
+	printTable();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
