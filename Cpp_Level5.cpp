a6af75c39e0231c5bde8cc5a00c7be9423709032
Tarik <tarik.jaidani@gmail.com>
2025-11-02 22:30:14 +0100

    3

--- Cpp_Level5.cpp
+++ Cpp_Level5.cpp
@@ -2,40 +2,18 @@
 #include <string>
 using namespace std;
  
-enum enPrimeNotPrim {Prime=1 , NotPrime=2};
+enum enPerfectNotPerfect {Perfect=2 , NotPerfect=2};
 
-int readNumber() {
+int readNumber(string Message) {
 	int Number;
 	do {
-		cout << "Please input a number: " << endl;
+		cout << Message << endl;
 		cin >> Number;
-	} while(Number <= 0);
+	} while (Number <= 0);
 	return Number;
-}
-
-
-enPrimeNotPrim checkPrime(int Number) {
-	int M = round(Number / 2);
-
-	for (int counter = 2; counter <= M; counter++)
-	{
-		if (Number % counter == 0)
-			return enPrimeNotPrim::NotPrime;
-	}
-	return enPrimeNotPrim::Prime;
-}
-
-void printPrime(int Number) {
-	for (int i = 1; i <= Number; i++) {
-		switch (checkPrime(i)) {
-		case  enPrimeNotPrim::Prime:
-			cout << i << endl;
-		}
-	}
 }
 
 int main()
 {
-	printPrime(readNumber());
-	 
+	readNumber("Merci d'inserer votre nombre: ");
 }
