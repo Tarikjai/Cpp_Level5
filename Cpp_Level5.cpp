8992308e44311c3c0127ebe110724f46a2225b2c
Tarik <tarik.jaidani@gmail.com>
2025-11-16 13:32:42 +0100

    push

--- Cpp_Level5.cpp
+++ Cpp_Level5.cpp
@@ -4,16 +4,42 @@ using namespace std;
  
 
 void printHeader() {
-	cout << "\t\tMultiplication Table From 1 to 1 " << endl;
+	cout << "\t\t\t\tMultiplication Table From 1 to 1 " << endl;
+	cout << endl;
+	for (int i = 1; i <= 10; i++) {
+		cout << " \t " << i ;
+	}
+	cout << endl;
+	cout << "____________________________________________________________________________________ " << endl;
+
+}
+
+void printTable() {
+	 
+
+	for (int i = 1;i <= 10; i++) {
+
+			if (i < 10)
+			cout << i << "     |  " ;
+			else 
+				cout << i << "    |  ";
+			for (int j = 1;j <= 10; j++) {
+				cout << i * j << " \t ";
+				
+			}
+			cout << endl;
+	
+
+		
+	}
+	
 
 }
 
 
-
-
-
 int main() {
 	printHeader();
+	printTable();
 }
 
 
