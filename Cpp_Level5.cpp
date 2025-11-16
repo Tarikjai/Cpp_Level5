0f18cfd0f31157ceed38277c65810891bd28fe2b
Tarik <tarik.jaidani@gmail.com>
2025-11-16 12:49:12 +0100

    push

--- Cpp_Level5.cpp
+++ Cpp_Level5.cpp
@@ -2,18 +2,64 @@
 #include <string>
 using namespace std;
  
-enum enPerfectNotPerfect {Perfect=2 , NotPerfect=2};
 
-int readNumber(string Message) {
-	int Number;
-	do {
-		cout << Message << endl;
-		cin >> Number;
-	} while (Number <= 0);
-	return Number;
+void printHeader() {
+	cout << "\t\tMultiplication Table From 1 to 1 " << endl;
+
 }
 
-int main()
-{
-	readNumber("Merci d'inserer votre nombre: ");
+
+
+
+
+int main() {
+	printHeader();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//enum enPerfectNotPerfect {Perfect=2 , NotPerfect=2};
+//
+//int readNumber(string Message) {
+//	int Number;
+//	do {
+//		cout << Message << endl;
+//		cin >> Number;
+//	} while (Number <= 0);
+//	return Number;
+//}
+//
+//int main()
+//{
+//	readNumber("Merci d'inserer votre nombre: ");
+//}
