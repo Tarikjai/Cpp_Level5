9472459c0b3682d437832c5f3fb4bcc4044a999b
Tarik <tarik.jaidani@gmail.com>
2025-12-23 22:58:31 +0100

    push

--- Cpp_Level5.cpp
+++ Cpp_Level5.cpp
@@ -1,6 +1,22 @@
 #include <iostream>   
 using namespace std;
 
+
+
+enum enPrimeNotPrim { Prime = 1, NotPrime = 2 };
+
+enPrimeNotPrim checkPrime(int Number) {
+    int M = round(Number / 2);
+
+    for (int counter = 2; counter <= M; counter++)
+    {
+        if (Number % counter == 0)
+            return enPrimeNotPrim::NotPrime;
+    }
+    return enPrimeNotPrim::Prime;
+}
+
+
 int RandomNumber(int From, int To)
 {
     int randNum = rand() % (To - From + 1) + From;
@@ -16,26 +32,41 @@ void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
         arr[i] = RandomNumber(1, 100);
 }
 
+
+
+void CopyOnlyPrimeNumbers(int arr[100], int arr2[100], int arrLength, int& arr2Length)
+{
+    
+    for (int i = 0; i < arrLength; i++) {
+       
+        if (checkPrime(arr[i])== enPrimeNotPrim::Prime) {
+            arr2[arr2Length] = arr[i];
+            arr2Length++;
+        } 
+    }
+    cout << "\n";
+}
+
+
+
 void PrintArray(int arr[100], int arrLength)
 {
-
     for (int i = 0; i < arrLength; i++)
         cout << arr[i] << " ";
-    cout << "\n";
+
 }
 
-void copyArray(int arr[100], int arr2[100], int arrLength)
+
+void PrintPrimeArray(int arr2[100], int arr2Length)
 {
+    cout << "\nPrime Numbers in Array2 : ";
+    for (int i = 0; i < arr2Length; i++) {
 
-    for (int i = 0; i < arrLength; i++) {
-        arr2[i] = arr[i];
-       // cout << arr2[i] << " ";
+        cout << arr2[i] << " ";
+
     }
-       
     cout << "\n";
-
 }
-
 
 
 
@@ -50,15 +81,17 @@ int main() {
 
     FillArrayWithRandomNumbers(arr, arrLength);
 
-    cout << "\nArray Elements: ";
-    PrintArray(arr, arrLength);
-
-    int arr2[100];
-    copyArray(arr, arr2, arrLength);
-
-    cout << "\nArray 2 elements after copy : ";
     
 
-    PrintArray(arr2, arrLength);
-    return 0;  // Return 0 to indicate successful program execution.
+    int arr2[100];
+    int arr2Length=0;
+
+    CopyOnlyPrimeNumbers(arr, arr2, arrLength, arr2Length);
+
+
+    cout << "\nArray 1 Elements: ";
+    PrintArray(arr, arrLength);
+    cout << "\nArray 2 Elements: ";
+    PrintArray(arr2, arr2Length);
+    return 0;  
 }
