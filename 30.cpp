0bd39ba75df638f3cfb603a2eba5da6672b43f06
Tarik <tarik.jaidani@gmail.com>
2025-12-24 00:06:42 +0100

    push

--- 30.cpp
+++ 30.cpp
@@ -21,25 +21,25 @@ int RandomNumber(int From, int To)
 
 void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
 {
-  
-    
+
+
     for (int i = 0; i < arrLength; i++)
         arr[i] = RandomNumber(1, 100);
 }
- 
+
 void PrintArray(int arr[100], int arrLength)
 {
     for (int i = 0; i < arrLength; i++) {
         cout << arr[i] << " ";
     }
 }
- 
+
 void sumArrays(int arr[100], int arr2[100], int arrLength, int SumArr[100]) {
     for (int i = 0; i < arrLength; i++) {
         SumArr[i] = arr[i] + arr2[i];
         cout << SumArr[i] << " ";
     }
-   
+
 }
 
 
@@ -47,7 +47,7 @@ void sumArrays(int arr[100], int arr2[100], int arrLength, int SumArr[100]) {
 int main() {
     srand((unsigned)time(NULL));
 
-    int arr[100] ;
+    int arr[100];
     int arr2[100];
     int SumArr[100];
 
@@ -55,18 +55,18 @@ int main() {
 
     FillArrayWithRandomNumbers(arr, arrLength);
     FillArrayWithRandomNumbers(arr2, arrLength);
-    
+
     //CopyOnlyPrimeNumbers(arr, arr2, arrLength, arr2Length);
 
 
     cout << "\nArray 1 Elements: ";
     PrintArray(arr, arrLength);
-    cout << "\nArray 2 Elements: " ;
+    cout << "\nArray 2 Elements: ";
     PrintArray(arr2, arrLength);
     cout << "  " << endl;
 
     cout << "\nSum of array1 and array2 elements: ";
     cout << "  " << endl;
     sumArrays(arr, arr2, arrLength, SumArr);
-    return 0;  
+    return 0;
 }
