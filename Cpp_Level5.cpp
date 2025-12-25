3d88f668a30490ce98c8b7b0ae10aef952c809ef
Tarik <tarik.jaidani@gmail.com>
2025-12-25 23:29:37 +0100

    push

--- Cpp_Level5.cpp
+++ Cpp_Level5.cpp
@@ -1,44 +1,35 @@
 #include <iostream>  
 using namespace std;
 
-int getPositiveNumber(string Message) {
+int ReadNumber(string Message) {
     int number = 0;
-    while (number <= 0) {
+   // while (number <= 0) {
         cout << Message << endl;
         cin >> number;
-    }
+   // }
     return number;
 }
 
-int RandomNumber(int From, int To) {
-    int randNum = rand() % (To - From + 1) + From;
-    return randNum;
+int AddArrayElement(int number ,int arr[100], int& arrLength) {
+    arrLength++;
+    arr[arrLength-1]= number ;
+   
+    return number;
 }
 
-short FindNumberPositionInArray(int arr[100], int arrLength, int digit) {
-    for (int i = 0; i < arrLength; i++) {
-        if (digit == arr[i]) {
-           
-            return   i;
-        }
- 
-    }
-    return -1;
+void InputUserNumbersInArray(int arr[100], int& arrLength) {
+    
+    
+    bool AddMore = true ;
+
+    do {
+        AddArrayElement(ReadNumber("Please enter a number: "), arr,  arrLength);
        
-}
-
-bool IsNumberInArray(int arr[100], int arrLength, int digit) {
+        cout << "Do you want to add more numbers? [0]:No, [1]:Yes ? ";
+        cin >> AddMore;
+    } while (AddMore) ;
      
-    return FindNumberPositionInArray(arr, arrLength, digit) != -1;
-
-}
-
-
-
-void fillNumber(int arr[100], int arrLength) {
-    for (int i = 0; i < arrLength; i++) {
-        arr[i] = RandomNumber(1, 100);
-    }
+  
 }
 
 void PrintArray(int arr[100], int arrLength) {
@@ -48,32 +39,15 @@ void PrintArray(int arr[100], int arrLength) {
 }
 
 int main() {
-    srand((unsigned)time(NULL));
+   
 
     int arr[100];
-    int arrLength = getPositiveNumber(" ");
+    int arrLength=0;
 
-    fillNumber(arr, arrLength);
-
-    cout << "\nArray 1 Elements: ";
+    InputUserNumbersInArray(arr, arrLength);
+    cout << "\nArray lenght: " << arrLength;
+    cout << "\nArray elements: ";
+   
     PrintArray(arr, arrLength);
-
-    int digit = getPositiveNumber("\n\nPlease enter a number to search for ?");
-   // ShuffleArray(arr, arrLength);
-
-    cout << "\nNumber you are looking for is:  " << digit << "\n";
-     
-
-    if (IsNumberInArray(arr, arrLength, digit)) {
-
-       
-        cout << "\nYes The number is  found :-)\n";
-    }
-    else {
-     
-        cout << "\nNo The number is not found :-(\n";
-    }
- 
-
-    return 0;
+    cout << "\n ";
 }
