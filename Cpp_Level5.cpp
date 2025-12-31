8c8e8e109e799dee8472106a51a9dd4fc2873d73
Tarik <tarik.jaidani@gmail.com>
2025-12-31 21:18:13 +0100

    push

--- Cpp_Level5.cpp
+++ Cpp_Level5.cpp
@@ -1,95 +1,89 @@
-#include <iostream>   
+#include <iostream>  
 using namespace std;
 
 
-enum enChartype { SmallLetter = 1, CapitalLetter = 2, SpecialCharacter = 3, Digit = 4 };
 
-int getPositiveNumber(string Message) {
-    int Number;
-    do {
-        cout << Message;
-        cin >> Number;
-    } while (Number <= 0);
-    return Number;
-}
 
-int RandomNumber(int From, int To) {
-    int RandomNumber = rand() % (To - From + 1) + From;                // génère un entier pseudo-aléatoire
-    return RandomNumber;
-}
- 
-char GetRandomCharachter(enChartype charType) {
 
-	switch (charType)
-	{
-	case enChartype::SmallLetter:
-		return char(RandomNumber(97, 122));
 
-	case enChartype::CapitalLetter:
-		return char(RandomNumber(65, 90));
 
-	case enChartype::SpecialCharacter:
-		return char(RandomNumber(33, 47));
+void ReadArray(int arrSource[100], int& Sourcelength) {
+    Sourcelength = 10;
 
-	case enChartype::Digit:
-		return char(RandomNumber(48, 57));
-
-	}
-}
-
-string GenerateWord(enChartype charType, int length) {
-
-	string Word;
-
-	for (int i = 1; i <= length; i++) {
-
-		Word = Word + char(GetRandomCharachter(charType));
-
-	}
-	return Word;
+    arrSource[0] = 10;
+    arrSource[1] = 10;
+    arrSource[2] = 10;
+    arrSource[3] = 50;
+    arrSource[4] = 50;
+    arrSource[5] = 70;
+    arrSource[6] = 70;
+    arrSource[7] = 70;
+    arrSource[8] = 70;
+    arrSource[9] = 90;
 }
 
 
-string GenerateKey() {
-	string Key = "";
 
-	Key = GenerateWord(enChartype::CapitalLetter, 4) + "-";
-	Key = Key + GenerateWord(enChartype::CapitalLetter, 4) + "-";
-	Key = Key + GenerateWord(enChartype::CapitalLetter, 4) + "-";
-	Key = Key + GenerateWord(enChartype::CapitalLetter, 4);
-
-	return Key;
-
-
+short findNumberPosition(int Number, int arrSource[100], int& Sourcelength) {
+    for (int i = 0; i < Sourcelength; i++) {
+        if (arrSource[i] == Number) {
+            return i;
+       }
+    }
+     return -1;       
 }
 
-void FillArrayWithRandomNumbers(string arrSource[100], int& Sourcelength) {
-    for (char i =0; i < Sourcelength; i++) {
-        arrSource[i] = GenerateKey();
+void AddItemmToArray(int Number, int arrSource[100], int& Sourcelength) {
+    Sourcelength++;
+    arrSource[Sourcelength - 1] = Number;
+}
+
+
+
+bool IsNumberInArray(int Number, int arrSource[100], int& Sourcelength) {
+    return findNumberPosition( Number,  arrSource,  Sourcelength) != -1;
+}
+
+
+void copyDistantNumber(int arrSource[100], int Sourcelength, int arrDestination[100], int& Destinationlength) {
+    for (int i = 0; i < Sourcelength; i++) {
+
+        if (!IsNumberInArray(arrSource[i], arrDestination, Destinationlength)) {
+            AddItemmToArray(arrSource[i], arrDestination, Destinationlength);
+        }
+        
     }
 }
 
-
-void PrintArray(string arrSource[100], int Number) {
-    for (char i = 0; i < Number; i++) {
-        cout <<"Array ["<<i+1 << "] : " << arrSource[i] << " \n" ;
+void PrintArray(int arrSource[100], int Sourcelength) {
+    for (int i = 0; i < Sourcelength; i++) {
+        cout << arrSource[i] << " ";
     }
-    cout << "\n";
 }
- 
-
 
 int main() {
-    srand((unsigned)time(NULL));
 
-	string arrSource[100];
-    int Sourcelength = getPositiveNumber("");
-     
-	FillArrayWithRandomNumbers( arrSource, Sourcelength);
- 
-    cout << "Array elements : \n\n";
+    int arrSource[100];
+    int Sourcelength;
+    int arrDestination[100];
+    int Destinationlength = 0;
+
+
+    ReadArray(arrSource, Sourcelength);
+    
+    copyDistantNumber(arrSource, Sourcelength, arrDestination, Destinationlength);
+
+    cout << "Array elements : ";
+
     PrintArray(arrSource, Sourcelength);
-  
-   
-    cout << "\n";
+
+    cout << "\nArray 2 : ";
+
+    PrintArray(arrDestination, Destinationlength);
+    cout << endl;
+
+
+
+
+
 }
