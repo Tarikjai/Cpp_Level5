87ad21d73f94f2b04ab077987f47145a731e88c5
Tarik <tarik.jaidani@gmail.com>
2026-01-01 20:09:38 +0100

    push

---
+++ 41.cpp
@@ -0,0 +1,70 @@
+#include <iostream>  
+using namespace std;
+
+
+
+void FillArray(int arrSource[100], int& Sourcelength) {
+    Sourcelength = 6;
+
+    arrSource[0] = 10;
+    arrSource[1] = 20;
+    arrSource[2] = 30;
+    arrSource[3] = 30;
+    arrSource[4] = 20;
+    arrSource[5] = 10;
+}
+
+
+
+
+
+void PrintArray(int arrSource[100], int Sourcelength) {
+    for (int i = 0; i < Sourcelength; i++) {
+        cout << arrSource[i] << " ";
+    }
+}
+
+bool IsPalindromeArray(int arrSource[100], int Sourcelength) {
+    for (int i = 0; i < Sourcelength; i++) {
+        if (arrSource[i] != arrSource[Sourcelength - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+
+
+}
+
+int main() {
+
+    int arrSource[100];
+    int Sourcelength;
+
+
+
+    FillArray(arrSource, Sourcelength);
+
+
+    cout << "Array elements : ";
+
+    PrintArray(arrSource, Sourcelength);
+
+
+
+
+    cout << "\n";
+    if (IsPalindromeArray(arrSource, Sourcelength) != 0)
+        cout << "\nYes , array is a Palindrome.\n";
+
+    else
+        cout << "\nNo , it is NOT a Palindrome number.\n";
+
+
+
+    cout << endl;
+
+
+
+
+
+}
--- Cpp_Level5.cpp
+++ Cpp_Level5.cpp
@@ -3,57 +3,20 @@ using namespace std;
 
 
 
-
-
-
-
-void ReadArray(int arrSource[100], int& Sourcelength) {
-    Sourcelength = 10;
+void FillArray(int arrSource[100], int& Sourcelength) {
+    Sourcelength = 6;
 
     arrSource[0] = 10;
-    arrSource[1] = 10;
-    arrSource[2] = 10;
-    arrSource[3] = 50;
-    arrSource[4] = 50;
-    arrSource[5] = 70;
-    arrSource[6] = 70;
-    arrSource[7] = 70;
-    arrSource[8] = 70;
-    arrSource[9] = 90;
+    arrSource[1] = 20;
+    arrSource[2] = 30;
+    arrSource[3] = 30;
+    arrSource[4] = 20;
+    arrSource[5] = 10;
 }
 
 
 
-short findNumberPosition(int Number, int arrSource[100], int& Sourcelength) {
-    for (int i = 0; i < Sourcelength; i++) {
-        if (arrSource[i] == Number) {
-            return i;
-       }
-    }
-     return -1;       
-}
 
-void AddItemmToArray(int Number, int arrSource[100], int& Sourcelength) {
-    Sourcelength++;
-    arrSource[Sourcelength - 1] = Number;
-}
-
-
-
-bool IsNumberInArray(int Number, int arrSource[100], int& Sourcelength) {
-    return findNumberPosition( Number,  arrSource,  Sourcelength) != -1;
-}
-
-
-void copyDistantNumber(int arrSource[100], int Sourcelength, int arrDestination[100], int& Destinationlength) {
-    for (int i = 0; i < Sourcelength; i++) {
-
-        if (!IsNumberInArray(arrSource[i], arrDestination, Destinationlength)) {
-            AddItemmToArray(arrSource[i], arrDestination, Destinationlength);
-        }
-        
-    }
-}
 
 void PrintArray(int arrSource[100], int Sourcelength) {
     for (int i = 0; i < Sourcelength; i++) {
@@ -61,25 +24,43 @@ void PrintArray(int arrSource[100], int Sourcelength) {
     }
 }
 
+bool IsPalindromeArray(int arrSource[100], int Sourcelength) {
+    for (int i = 0; i < Sourcelength; i++) {
+        if (arrSource[i] != arrSource[Sourcelength - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+
+
+}
+
 int main() {
 
     int arrSource[100];
     int Sourcelength;
-    int arrDestination[100];
-    int Destinationlength = 0;
+ 
 
 
-    ReadArray(arrSource, Sourcelength);
+    FillArray(arrSource, Sourcelength);
     
-    copyDistantNumber(arrSource, Sourcelength, arrDestination, Destinationlength);
-
+   
     cout << "Array elements : ";
 
     PrintArray(arrSource, Sourcelength);
 
-    cout << "\nArray 2 : ";
 
-    PrintArray(arrDestination, Destinationlength);
+   
+
+    cout << "\n";
+    if (IsPalindromeArray(arrSource, Sourcelength)!= 0)
+        cout << "\nYes , array is a Palindrome.\n";
+        
+    else
+        cout << "\nNo , it is NOT a Palindrome number.\n";    
+    
+
+
     cout << endl;
 
 
