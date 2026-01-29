8c6253e15f8692be846a334338abd0eb990443b4
Tarik <tarik.jaidani@gmail.com>
2026-01-29 23:13:52 +0100

    push

--- Cpp_Level5.cpp
+++ Cpp_Level5.cpp
@@ -2,46 +2,20 @@
 #include <conio.h>
 #include <string>
 using namespace std;
-enum enGameChoice {	Stone = 1, Papper = 2, Scissors = 3};
-enum enWinner { Player = 1, Computer = 2, Draw = 3 };
 
-struct stGameResult {};
-struct stRoundResult {};
+enum enGameChoice { Stone = 1, Papper = 2, Sicssors = 3 };
+enum enGameWinner {Player=1, Computer=2, Draw=3};
 
-string Tab(short Numbert) {
-	string t = "\t";
-	for (int i = 0; i < Numbert; i++) {
-		t = t + "\t";
-	}
-	return t;
-}
+struct stRoundResult {
+	short Round;
 
-stGameResult playGame() {
+};
 
 
-}
-
-
-
-
-void startGame(stGameResult GameResutl) {
-
-	string PlayGame = "Y";
-	do {
-		playGame();
-
-
-		cout << Tab(2) << "Do You want to play again Y/N?";
-		cin >> PlayGame;
-
-	} while (PlayGame == "Y" || PlayGame == "y");
-
-
-}
 
 
 
 int main() {
 
-	startGame();
+	StartGame();
 }
