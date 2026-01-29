6ab46c0067c527bebe3e6c82245cdd8ddecc41eb
Tarik <tarik.jaidani@gmail.com>
2026-01-29 23:44:24 +0100

    push

--- Cpp_Level5.cpp
+++ Cpp_Level5.cpp
@@ -4,18 +4,36 @@
 using namespace std;
 
 enum enGameChoice { Stone = 1, Papper = 2, Sicssors = 3 };
-enum enGameWinner {Player=1, Computer=2, Draw=3};
+enum enWinner {Player=1, Computer=2, Draw=3};
 
-struct stRoundResult {
-	short Round;
+struct stRoundinfo {
+	short Round=0;
+	enGameChoice PlayerChoice;
+	enGameChoice ComputerChoice;
+	enWinner Winner;
+	string WinnerName = ""; 
 
 };
+
+struct stGameResult {
+	short RoundNumber = 0;
+	short PlayerWin = 0;
+	short ComputerWin = 0;
+	short Draw = 0;
+	enWinner Winner;
+	string WinnerName = "";
+};
+
+
+stGameResult StartGame() {
+
+}
 
 
 
 
 
 int main() {
-
+	srand((unsigned)time(NULL));
 	StartGame();
 }
